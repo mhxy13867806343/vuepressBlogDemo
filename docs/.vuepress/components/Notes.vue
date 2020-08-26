99ac7840cf57bd920a3321ded00bde93164454b7
ningbohanbaoge <liuchunsong@hanbaoyufang.com>
2020-08-26 21:17:23 +0800

    又加内容吧

--- docs/.vuepress/components/Notes.vue
+++ docs/.vuepress/components/Notes.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="content">
     <p v-for="(item,index) in list" :key="index">
-      <el-link :href="item.href" target="_blank" :title="item.name">{{item.name}}<i class="el-icon-view el-icon--right"></i></el-link>
+      <el-link  :underline="false" :href="item.href" target="_blank" :title="item.name">{{item.name}}<i class="el-icon-view el-icon--right"></i></el-link>
     </p>
   </div>
 </template>
@@ -15,6 +15,14 @@ export default {
         {
           name:'es6基础总结01之let,var,count的理解',
           href:'https://www.yuque.com/jscode/notes/xvpwbh'
+        },
+        {
+          name:'使用 uni-app 开发微信小程序的实践 + 踩坑合集',
+          href:'https://www.yuque.com/jscode/notes/ol48er'
+        },
+        {
+          name:'vue项目之(pc端)后台管理系统开发',
+          href:'https://www.yuque.com/jscode/notes/ceboy2'
         }
       ]
     }
