4dafd8628c51acb25f5e6a516e398df5caec9e1f
hooks vue <hooksvue@hooksdeMacBook-Pro.local>
2022-06-27 10:52:24 +0800

    add

--- scripts/deploy-gh.sh
+++ scripts/deploy-gh.sh
@@ -14,15 +14,15 @@ cd docs/.vuepress/dist
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'vuepressBlogDemo'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:mhxy13867806343/mhxy13867806343.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# git push -f git@github.com:mhxy13867806343/mhxy13867806343.git master:gh-pages
 
 # 把上面的 <USERNAME> 换成你自己的 Github 用户名，<REPO> 换成仓库名，比如我这里就是：
-git push -f git@github.com:wtyqer/blog.git master:gh-pages
+git push -f git@github.com:mhxy13867806343/mhxy13867806343.git master:vuepressBlogDemo
 
 cd -
